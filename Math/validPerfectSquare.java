250f8c4ba03b6f05a62d11b374c66e35f340a713
Param Mehta <param.mehta95@gmail.com>
2017-10-25 18:09:06 -0700

    Valid Perfect Square

---
+++ Math/validPerfectSquare.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class validPerfectSquare {
+    private static boolean isPerfectSquare(int num) {
+        int i = 1;
+        while (num > 0) {
+            num -= i;
+            i += 2;
+        }
+        return num == 0;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int num = scanner.nextInt();
+        System.out.println(isPerfectSquare(num));
+    }
+}
+
