3c188eb060db8c9e816c1e40293c9798ffdab7e9
Param Mehta <param.mehta95@gmail.com>
2017-10-26 16:55:57 -0700

    Code Fights

---
+++ Code Fights/Arrays/firstNonRepeatingCharacter.java
@@ -0,0 +1,65 @@
+/*
+    Note: Write a solution that only iterates over the string once and uses O(1) additional memory, since this is what you would be asked to do during a real interview.
+
+    Given a string s, find and return the first instance of a non-repeating character in it. If there is no such character, return '_'.
+
+    Example
+
+    For s = "abacabad", the output should be
+    firstNotRepeatingCharacter(s) = 'c'.
+
+    There are 2 non-repeating characters in the string: 'c' and 'd'. Return c since it appears in the string first.
+
+    For s = "abacabaabacaba", the output should be
+    firstNotRepeatingCharacter(s) = '_'.
+
+    There are no characters in this string that do not repeat.
+
+    Input/Output
+
+    [time limit] 3000ms (java)
+    [input] string s
+
+    A string that contains only lowercase English letters.
+
+    Guaranteed constraints:
+    1 ≤ s.length ≤ 105.
+
+    [output] char
+
+    The first non-repeating character in s, or '_' if there are no characters that do not repeat.
+*/
+
+import java.util.HashMap;
+
+class firstNonRepeatingCharacter {
+
+    static char character(String s) {
+
+        HashMap<Character, Integer> charMap = new HashMap<Character, Integer>();
+        int i, length;
+        Character c;
+        length = s.length();
+        for (i = 0; i < length; i++) {
+            c = s.charAt(i);
+            if (charMap.containsKey(c)) {
+                charMap.put(c, charMap.get(c) + 1);
+            } else
+                charMap.put(c, 1);
+        }
+
+        for (i = 0; i < length; i++) {
+            c = s.charAt(i);
+            if (charMap.get(c) == 1)
+                return c;
+        }
+        return '_';
+
+    }
+
+    public static void main(String[] args) {
+        String str = "abacabaabacaba";
+        System.out.println(character(str));
+    }
+
+}
---
+++ Code Fights/Arrays/rotateImage.java
@@ -0,0 +1,32 @@
+import java.util.Arrays;
+
+public class rotateImage {
+
+    static int[][] rotateImage(int[][] a) {
+
+        int n, i, j;
+        n = a.length;
+
+        for (i = 0; i < n / 2; i++) {
+            for (j = i; j < n - i - 1; j++) {
+                int temp = a[i][j];
+                a[i][j] = a[n - j - 1][i];
+                a[n - j - 1][i] = a[n - 1 - i][n - 1 - j];
+                a[n - 1 - i][n - 1 - j] = a[j][n - 1 - i];
+                a[j][n - 1 - i] = temp;
+            }
+        }
+        return a;
+    }
+
+    public static void main(String[] args) {
+        int[][] arr = new int[][]
+                {
+                        {1, 2, 3},
+                        {4, 5, 6},
+                        {7, 8, 9}
+                };
+        System.out.println(Arrays.deepToString(rotateImage(arr)));
+    }
+
+}
