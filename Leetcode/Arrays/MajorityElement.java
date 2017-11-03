4ebed7d51546291ab7fa2f49c9f2da8bac8bef75
Param Mehta <param.mehta95@gmail.com>
2017-11-03 10:26:43 -0700

    Day 1

--- Leetcode/Arrays/MajorityElement.java
+++ Leetcode/Arrays/MajorityElement.java
@@ -1,3 +1,9 @@
+/*
+Given an array of size n, find the majority element. The majority element is the element that appears more than ⌊ n/2 ⌋ times.
+
+You may assume that the array is non-empty and the majority element always exist in the array.
+ */
+
 public class MajorityElement {
     public static void main(String[] args) {
         int[] nums = new int[]{1, 2, 2, 2, 3};
