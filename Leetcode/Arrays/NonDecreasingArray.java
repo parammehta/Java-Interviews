4b0272eb663827cf8fe3f1a0ec6047ef2ab346a1
Param Mehta <param.mehta95@gmail.com>
2017-11-06 14:20:40 -0700

    Non-Decreasing Array

--- Leetcode/Arrays/NonDecreasingArray.java
+++ Leetcode/Arrays/NonDecreasingArray.java
@@ -1,3 +1,8 @@
+/*
+    Given an array with n integers, your task is to check if it could become non-decreasing by modifying at most 1 element.
+
+    We define an array is non-decreasing if array[i] <= array[i + 1] holds for every i (1 <= i < n).
+ */
 public class NonDecreasingArray {
 
     public static void main(String[] args) {
@@ -7,14 +12,17 @@ public class NonDecreasingArray {
     }
 
     public boolean checkPossibility(int[] nums) {
-        int count = 0;                                                                    //the number of changes
+        //the number of changes
+        int count = 0;
         for (int i = 1; i < nums.length && count <= 1; i++) {
             if (nums[i - 1] > nums[i]) {
                 count++;
                 if (i - 2 < 0 || nums[i - 2] <= nums[i])
-                    nums[i - 1] = nums[i];                    //modify nums[i-1] of a priority
+                    //modify nums[i-1] of a priority
+                    nums[i - 1] = nums[i];
                 else
-                    nums[i] = nums[i - 1];                                                //have to modify nums[i]
+                    //have to modify nums[i]
+                    nums[i] = nums[i - 1];
             }
         }
         return count <= 1;
