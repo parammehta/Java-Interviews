cdbfc25b8c47231a1858529de831c82c1e805c2b
Param Mehta <param.mehta95@gmail.com>
2018-02-06 11:31:45 -0700

    Leetcode Explore - Arrays

---
+++ Leetcode/Arrays/PlusOne.java
@@ -0,0 +1,23 @@
+/*
+    Given a non-negative integer represented as a non-empty array of digits, plus one to the integer.
+
+    You may assume the integer do not contain any leading zero, except the number 0 itself.
+
+    The digits are stored such that the most significant digit is at the head of the list.
+*/
+public class PlusOne {
+    public int[] plusOne(int[] digits) {
+        int length = digits.length;
+        int j = length;
+        for (int i = length; i > 0; i--) {
+            if (digits[i - 1] < 9) {
+                digits[i - 1] = digits[i - 1] + 1;
+                return digits;
+            }
+            digits[i - 1] = 0;
+        }
+        int[] newNumber = new int[length + 1];
+        newNumber[0] = 1;
+        return newNumber;
+    }
+}
